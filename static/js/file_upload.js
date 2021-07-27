e79506c4d026a2c23d1dd5b60fde91064abf09e2
Shuchir <53063247+CoolCoderSJ@users.noreply.github.com>
2021-07-27 15:36:55 -0400

    Initial Commit

---
+++ static/js/file_upload.js
@@ -0,0 +1,55 @@
+document.getElementById("paste-upload").addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	document.getElementById("output").classList = "hidden";
+	document.getElementById("loader").classList = "";
+	document.getElementById("image-container").classList = "";
+	document.getElementById("close").classList = "hidden";
+
+	
+	fetch("/api/"+document.getElementById("file-format").getAttribute("data-selected").toLowerCase(),
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(document.querySelector("#paste-upload textarea").value)
+		}
+	)
+	.then(response => response.blob())
+	.then(blob => {
+		document.getElementById("output").src = URL.createObjectURL(blob);
+		document.getElementById("loader").classList = "hidden";
+		document.getElementById("output").classList = "";
+		document.getElementById("close").classList = "";
+	});
+});
+
+document.getElementById("file-input").addEventListener("change", (e) => {
+	document.getElementById("output").classList = "hidden";
+	document.getElementById("loader").classList = "";
+	document.getElementById("image-container").classList = "";
+
+	const file = e.target.files[0];
+	const fr = new FileReader();
+	fr.onload = function(evt) {
+			fetch("/api/"+document.getElementById("file-format").getAttribute("data-selected").toLowerCase(),
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(evt.target.result)
+				}
+			)
+			.then(response => response.blob())
+			.then(blob => {
+				document.getElementById("output").src = URL.createObjectURL(blob);
+				document.getElementById("loader").classList = "hidden";
+				document.getElementById("output").classList = "";
+				document.getElementById("close").classList = "";
+			});
+	};
+
+	fr.readAsText(file, "UTF-8");
+});
